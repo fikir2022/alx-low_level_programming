2ec668914b3f5b865aca6b0c360ab64f18e3d6d3
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-06-02 14:21:23 -0700

    commit for the 5th task called :Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main -entry point
+ *main - Entry point
  *My hello, printf task 
  *Return: retirn 1 after program excution
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n"
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
