028f8b39b86b92c7097fc818d66588603f37e48d
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-06-05 01:39:00 -0700

    commit for the 10th task named : Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,14 +20,12 @@ putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 
 		
-if (digit1 == 8 && digit2 == 9)
-{	
+if (digit1 == 8 && digit2 == 9)	
 continue;
 }
 
 putchar(',');
 putchar(' ');
 
-return (0);
-}
+
 }
