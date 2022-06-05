586c8f106210ebaab6e5dda3295b38b498c88aa5
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-06-05 03:40:18 -0700

    commit for the 10th task named : Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,73 +1,29 @@
 #include <stdio.h>
 /**
-  * main - Prints combination of numbers
-  *
-  * Return: Always (Success)
-  */
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
+	int digit1, digit2;
 
-int c, i;
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-
-for (c = '0'; c <= '9'; c++)
-
-{
-
-		
-for (i = '0'; i <= '9'; i++)
-
-
-{
+			if (digit1 == 8 && digit2 == 9)
+				continue;
 			
-if (c < i)
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
+	putchar('\n');
 
-
-{
-
-
-
-
-putchar(c);
-putchar(i);
-
-
-
-
-
-if (c != '8' || (c == '8' && i != '9'))
-
-
-
-
-{
-
-
-
-
-
-putchar(',');
-putchar(' ');
-
-
-
-
+	return (0);
 }
-
-
-
-}
-
-
-}
-
-}
-
-	
-putchar('\n');
-
-	
-return (0);
-}
-
