a068f9349a98916db7e9da236fa931017ca98af9
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-06-07 14:29:18 -0700

    commit for the 12th task called : The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,8 +4,14 @@
  * @n: input
  * Return: Always 0 (Success)
  */
-void print_times_table(int n)
+int main(void)
 {
-
-	return (0);
+   print_times_table(3);
+   _putchar('n\');
+   print_times_table(5);
+   _putchar('n\');
+   print_times_table(98);
+    _putchar('n\');
+   print_times_table(12);
+   return (0);
 }
