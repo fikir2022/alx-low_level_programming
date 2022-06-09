b5323da3b8ff9f965643d88de7df63b63e9715aa
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-06-09 04:07:57 -0700

    task zero commit named :isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _isupper - evaluate if letter is uppercase .
- *@c:print int
- *Return:Always 0.
+ * _isupper - evalue if letter is uppercase .
+ *@c: print int
+ * Return: Always 0.
  */
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'z')
+if (c >= 'A' && c <= 'Z')
 {
 return (1);
 }
