0220d8860590a94d8131fb9a1d213c1f1cb844f3
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-06-09 05:25:09 -0700

    commit for the 12th task named : Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,29 @@
 #include "main.h"
 /**
- * print_number - that prints an integer
- *@n: print int
- *Return: Always 0.
+ * print_number - that prints an integr
+ * @n: print int
+ * Return: Always 0.
  */
 void print_number(int n)
 {
+
+   if (n < 0) {
+       putchar('-');
+       n = -n;
+   }
+
+   if (n == 0)
+      putchar('0');
+
+   if (n/10)
+      printnumber(n/10);
+
+   putchar(n%10 + '0');
+}
+
+int main(int argc, char** argv)
+{
+   int n = atoi(argv[1]);
+   printnumber(n);
+   printf("\n");
+}
