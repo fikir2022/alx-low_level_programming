e331c3f760c643478b2f9f3260356b1a23bb7691
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-06-10 02:08:32 -0700

    commit for the 1st task named :isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - if it is a digit .
+ *@c:print int
+ * Return: Always 0.
+ */
+
+int -isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+}
+
