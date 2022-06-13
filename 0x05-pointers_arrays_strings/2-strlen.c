6dd67ffda362d89bcf7821eafe9bb8ff6388309c
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-06-13 08:58:27 -0700

    commit for the 2nd task called :This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main."
+
+/**
+ *_strlen - string length
+ *@s: A pointer that will be changed to int
+ *
+ * Return: void means the answer is correct
+ */
+
+int _strlen(char *s)
+{
+int a;
+
+a = 0;
+while (s[a] != '\0')
+{
+a++;
+}
+return (a);
+}
