073e7ec7d0ba3610bce96fb7a7461a7440bf4599
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-06-13 07:09:29 -0700

    commit for 1st task called :Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of integers n and m
+ *@n: N pointer
+ *@m: M pointer
+ *Return: void means correct 
+ */
+
+void swap_int(int *n, int *m)
+{
+int o;
+
+o = *n;
+*n = *m;
+*m = o;
+}
