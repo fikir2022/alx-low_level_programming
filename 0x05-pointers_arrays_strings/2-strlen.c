ec8f32230ee9322cfc2a52effa022ff4bbd2c8a3
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-06-13 14:29:18 -0700

    commit for the 11th task called :Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,36 @@
-#include "main.h"
+#include 
+#include 
+#include 
 
-/**
- *_strlen - string length
- *@s: A pointer that will be changed to int
- *
- * Return: void means the answer is correct
- */
-
-int _strlen(char *s)
+int main()
 {
-int a;
-
-a = 0;
-while (s[a] != '\0')
-{
-a++;
-}
-return (a);
-}
+    char a[10],c[10],d;
+    int i,j,k=0;
+    printf("#Keygen by b44nz0r\n\n");
+    
+    while (k <5 || k >=10)
+    {
+          if (k !=0)
+             printf("\nThe username length should be 5 to 10 alphabets\n");
+        
+         printf("enter username: ");
+         scanf("%s",a);
+         k = strlen(a);
+    }
+    
+    i = k-1;
+    j = 0;
+    
+    while (i >= 0)
+    {
+          c[j] = a[i]+i;
+          i--;
+          j++;
+    }
+    
+    c[j] = 0;
+    printf("\nThe password is %s\n",c);
+    printf("\nHit Enter to Exit\n");
+    getchar();
+    getchar();
+} 
