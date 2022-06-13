a37560cb0c3b47b7e4b6a74061441c7639c0a3ae
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-06-13 13:41:25 -0700

    commit for the 11th task called :Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,17 @@
-#include "main.h"
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/**
- */
+
 int main(void)
 {
-int num;
+  int n;
+  int a[5];
+  int *p;
 
-srand(time(0));
-num = rand();
-printf("%i\n", num);
-return (0);
+  a[2] = 1024;
+  p = &n;
+  /**
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
