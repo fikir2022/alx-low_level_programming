158be975735e490aa8a240845cac1df6a2edfd5e
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-06-13 06:38:13 -0700

    task zero commit called :98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include :main.h"
+
+/**
+ *reset_to_98 - put an integer value to 98
+ *@n: A pointer that will be updated to an integer
+ *
+ * Return: void means correect
+ */
+
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}
+
