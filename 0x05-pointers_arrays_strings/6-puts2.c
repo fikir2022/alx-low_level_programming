91f1f4b566f712eb9918eda596c6779a43277ee6
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-06-13 12:34:40 -0700

    commit for the 6th task called :Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - prints every character of a string
+ * @str: A pointer will be changed to an int
+ * 
+ * Return: void means the answer is correct 
+ */
+
+void puts2(char *str)
+{
+int i;
+
+for (i = 0; str[a] !='\0'; i++)
+{
+if (i % 2 == 0)
+_putchar (str[i]);
+}
+
+_putchar ('\n');
+}
+
