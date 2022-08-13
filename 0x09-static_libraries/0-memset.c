8cd6d3c75381e2de35e1703fb43e018d8749fbdb
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-08-13 04:27:04 -0700

    0-memset.c

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+ *_memset -   fills memory with a constant byte
+ *@s: pointer block of memory to fill
+ *@b: value to set
+ *@n: bytes of the memory
+ *Return: dest
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+        unsigned int i;
+
+        /*Delacring FOR*/
+        for (i = 0; i < n; i++)
+        {
+                *(s + i) = b; /*add 1 position s*/
+
+        } /*END FOR*/
+
+        return (s);
+}
+
+0-strcat.c
+#include "main.h"
+/**
+ *_strcat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will also be changed
+ *Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
+
