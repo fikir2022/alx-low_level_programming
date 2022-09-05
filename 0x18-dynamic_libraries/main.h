27b74161bff7a5778e287e96af303b626fc17567
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-09-05 15:27:22 -0700

    main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <stdio.h>
-#include <stdlib.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -25,4 +23,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 
-#endif /* MAIN_H */
+#endif
